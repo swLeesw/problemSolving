87c5b1de86b4fd54a75099cb58e43fc6e511de15
SeungwonLee <74245930+swLeesw@users.noreply.github.com>
2024-03-10 14:03:00 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.83 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int solution(int n)
+{
+    int ans = 0;
+   
+    while (n > 0) {
+        if (n % 2 == 1) {
+            ans += 1;
+        }
+        n = n / 2;
+    }
+    return ans;
+}
+
